ae0a0ce56da6c4a55cb850d7066b02c1907d6203
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-22 01:58:21 +0200

    Stats section

--- src/components/home-page/stats-section/index.tsx
+++ src/components/home-page/stats-section/index.tsx
@@ -1,7 +1,23 @@
 const StatsSection = () => (
-    <section title="faq">
-        <div className="flex h-64 items-center justify-center bg-neutral-800">
+    <section title="faq" className="flex items-center justify-center">
+        {/* <div className="flex h-64 items-center justify-center bg-[#63f194]">
             <h2 className="text-lg text-white">Stats</h2>
+        </div> */}
+        <div className="flex w-full items-center justify-center border-r-2 border-black bg-[#63f194]">
+            <span className="pr-6 text-[21px]  font-light">Bounties</span>
+            <span className="text-[45px] font-medium">135</span>
+        </div>
+        <div className="flex w-full items-center justify-center border-r-2 border-black bg-[#63f194]">
+            <span className="pr-6 text-[21px]  font-light">Bounties</span>
+            <span className="text-[45px] font-medium">135</span>
+        </div>
+        <div className="flex w-full items-center justify-center border-r-2 border-black bg-[#63f194]">
+            <span className="pr-6 text-[21px]  font-light">Bounties</span>
+            <span className="text-[45px] font-medium">135</span>
+        </div>
+        <div className="flex w-full items-center justify-center bg-[#63f194]">
+            <span className="pr-6 text-[21px] font-light">Bounties</span>
+            <span className="text-[45px] font-medium">135</span>
         </div>
     </section>
 );
