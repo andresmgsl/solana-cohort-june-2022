f6ef167f77247d585a6c630d2996470054a19c9c
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-20 19:03:41 -0500

    Create Footer component

---
+++ src/components/common/layout/footer/index.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="mt-12 flex h-32 items-center justify-center bg-neutral-900 px-4">
+    <p className="text-white">Footer</p>
+  </footer>
+);
+
+export default Footer;
