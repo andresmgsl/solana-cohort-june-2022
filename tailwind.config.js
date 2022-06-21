f88ba70aed8a5319b30c059f859a80774afcb11b
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-21 13:22:44 -0500

    Run ESLint and Prettier; update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const typography = require('@tailwindcss/typography');
 
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'media',
-  theme: {
-    fontFamily: {
-      sans: ['Rubik'],
+    mode: 'jit',
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'media',
+    theme: {
+        fontFamily: {
+            sans: ['Rubik'],
+        },
+        screens: { ...defaultTheme.screens },
     },
-    screens: { ...defaultTheme.screens },
-  },
-  plugins: [typography],
+    plugins: [typography],
 };
