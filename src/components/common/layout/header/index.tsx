25f6cc95099d1eb05b0d0e85c57b3cca51341a2d
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-20 19:03:31 -0500

    Create Header component

---
+++ src/components/common/layout/header/index.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="sticky top-0 z-10 mb-20 flex h-16 items-center justify-center bg-neutral-900 px-4">
+    <h1 className="text-white">Header</h1>
+  </header>
+);
+
+export default Header;
