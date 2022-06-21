a52397fcb520a15bb4711082d90189921beac0a4
Hares Mahmood <hmahmood229@gmail.com>
2022-06-21 15:00:05 +0000

    Ensure correct structure in `Hero` section.

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <header className="absolute top-0 z-10 flex px-4 h-16 w-full items-center justify-center bg-black/25 backdrop-filter backdrop-blur-xl">
+  <header className="fixed top-0 z-10 flex px-4 h-16 w-full items-center justify-center bg-black/25 backdrop-filter backdrop-blur-xl">
     <h1 className="text-white">Header</h1>
   </header>
 );
--- src/components/home-page/hero-section/index.tsx
+++ src/components/home-page/hero-section/index.tsx
@@ -1,20 +1,19 @@
 import Button from 'components/common/button';
 
 const HeroSection = () => (
-  <section title="hero">
-    <div className="flex w-screen h-screen items-center justify-center bg-transparent">
-      <h1 className="text-lg text-white">Bounties.</h1>
-      <h1 className="text-lg text-white">Do you have what it takes?</h1>
-    </div>
-    <div className="subtitle">
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. d sed
+  <section title="hero" className="flex w-screen h-screen items-center justify-center bg-transparent">
+    <div className="flex flex-col gap-8">
+      <h1 className="text-6xl font-medium text-white">
+        Bounties.<br />Do you have what it takes?
+      </h1>
+      <p className="w-7/12 text-white text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ad sed
         adipiscing volutpat tincidunt amet vulputate porta est.
-      </h2>
-    </div>
-    <div className="buttonsContainer flex">
-      <Button text="Fund Bounty" variant="green" />
-      <Button text="Start Exploring" variant="transparent" />
+      </p>
+      <div className="flex gap-4">
+        <Button text="Fund Bounty" variant="green" />
+        <Button text="Start Exploring" variant="transparent" />
+      </div>
     </div>
   </section>
 );
