92eb7305b060862f0db68bb707ed1c38838410ec
William Wang <swang45@uw.edu>
2022-06-21 07:38:17 -0700

    Adde Hero Skeleton

--- src/components/home-page/hero-section/index.tsx
+++ src/components/home-page/hero-section/index.tsx
@@ -1,7 +1,20 @@
+import Button from 'components/common/button';
+
 const HeroSection = () => (
   <section title="hero">
-    <div className="flex h-64 items-center justify-center bg-neutral-800">
-      <h2 className="text-lg text-white">Hero</h2>
+    <div className="title flex h-64 items-center justify-center bg-neutral-800">
+      <h1 className="text-lg text-white">Bounties.</h1>
+      <h1 className="text-lg text-white">Do you have what it takes?</h1>
+    </div>
+    <div className="subtitle">
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. d sed
+        adipiscing volutpat tincidunt amet vulputate porta est.
+      </h2>
+    </div>
+    <div className="buttonsContainer flex">
+      <Button text="Fund Bounty" variant="green" />
+      <Button text="Start Exploring" variant="transparent" />
     </div>
   </section>
 );
