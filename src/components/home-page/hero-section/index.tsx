f6e8c254c1bbf8b52e318c3ec3a6ee198bb25997
William Wang <swang45@uw.edu>
2022-06-21 08:52:14 -0700

    Added Hero Image & Updated Next

--- src/components/home-page/hero-section/index.tsx
+++ src/components/home-page/hero-section/index.tsx
@@ -1,21 +1,30 @@
 import Button from 'components/common/button';
+import Image from 'next/image';
 
 const HeroSection = () => (
   <section title="hero">
-    <div className="title flex h-64 items-center justify-center bg-neutral-800">
-      <h1 className="text-lg text-white">Bounties.</h1>
-      <h1 className="text-lg text-white">Do you have what it takes?</h1>
+    <div className="relative z-50">
+      <div className="title flex h-64 items-center justify-center bg-neutral-800">
+        <h1 className="text-lg text-white">Bounties.</h1>
+        <h1 className="text-lg text-white">Do you have what it takes?</h1>
+      </div>
+      <div className="subtitle">
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. d sed
+          adipiscing volutpat tincidunt amet vulputate porta est.
+        </h2>
+      </div>
+      <div className="buttonsContainer flex">
+        <Button text="Fund Bounty" variant="green" />
+        <Button text="Start Exploring" variant="transparent" />
+      </div>
     </div>
-    <div className="subtitle">
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. d sed
-        adipiscing volutpat tincidunt amet vulputate porta est.
-      </h2>
-    </div>
-    <div className="buttonsContainer flex">
-      <Button text="Fund Bounty" variant="green" />
-      <Button text="Start Exploring" variant="transparent" />
-    </div>
+    <Image
+      className="z-0 w-screen sm:w-auto"
+      src="/hero.svg"
+      alt="hero background"
+      layout="raw"
+    />
   </section>
 );
 
