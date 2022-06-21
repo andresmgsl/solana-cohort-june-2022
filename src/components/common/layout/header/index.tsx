cfd79fc36414a948d25e87198fd03ac118a33a5f
William Wang <swang45@uw.edu>
2022-06-21 11:58:03 -0700

    Finished Hero & Header

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 const Header = () => (
-  <header className="fixed top-0 z-10 flex px-4 h-16 w-full items-center justify-center bg-black/25 backdrop-filter backdrop-blur-xl">
-    <h1 className="text-white">Header</h1>
+  <header className="fixed top-0 z-10 flex px-4 h-16 w-full items-center bg-black/25 backdrop-filter backdrop-blur-xl">
+    <Image src="/logo.svg" alt="Solana Logo" layout="fixed" width={175} height={100}></Image>
   </header>
 );
 
--- src/components/home-page/hero-section/index.tsx
+++ src/components/home-page/hero-section/index.tsx
@@ -4,22 +4,32 @@ import Image from 'next/image';
 const HeroSection = () => (
   <section
     title="hero"
-    className="relative flex h-screen w-screen items-center justify-center bg-black"
+    className="relative flex h-screen w-screen flex-col items-center justify-center bg-black"
   >
     <div
-      className="absolute z-0 -left-3/4 w-screen h-screen"
+      className="absolute -left-3/4 z-0 h-screen w-screen"
       style={{
         background:
           'radial-gradient(50% 50% at 50% 50%, rgb(106, 108, 253) 0%, rgba(0, 0, 0, 0) 100%)',
       }}
     ></div>
-    <div className="z-50 flex flex-col gap-8">
+    <div
+      style={{
+        background:
+          'radial-gradient(45.5% 47.88% at 45% 50.47%, rgb(255, 0, 199) 0%, rgba(0, 0, 0, 0) 100%)',
+      }}
+      className="z-0 w-screen relative sm:absolute top-32 sm:top-0 sm:right-0 sm:h-screen sm:w-1/2"
+    >
+      <Image src="/hero.svg" alt="hero background" layout="fill" />
+      <img className="invisible" src="/hero.svg" alt="placeholder"></img>
+    </div>
+    <div className="relative -top-12 z-50 flex flex-col gap-8 px-5 text-left sm:top-0 sm:-left-12">
       <h1 className="text-6xl font-medium text-white">
         Bounties.
         <br />
         Do you have what it takes?
       </h1>
-      <p className="w-7/12 text-lg text-white">
+      <p className="w-full text-lg text-white sm:w-7/12">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ad sed
         adipiscing volutpat tincidunt amet vulputate porta est.
       </p>
@@ -27,16 +37,6 @@ const HeroSection = () => (
         <Button text="Fund Bounty" variant="green" />
         <Button text="Start Exploring" variant="transparent" />
       </div>
-    </div>
-    <div
-      style={{
-        background:
-          'radial-gradient(45.5% 47.88% at 45% 50.47%, rgb(255, 0, 199) 0%, rgba(0, 0, 0, 0) 100%)',
-      }}
-      className="-top-1/5 absolute right-0 z-0 w-1/2"
-    >
-      <Image src="/hero.svg" alt="hero background" layout="fill" />
-      <img className="invisible" src="/hero.svg" alt="placeholder"></img>
     </div>
   </section>
 );
