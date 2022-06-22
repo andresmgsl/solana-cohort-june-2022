deff7be60a2cd1226a15afb8591acb79bd3102a3
hreyesm <hectorreyesmanrique@gmail.com>
2022-06-22 17:08:49 -0500

    Run Prettier

--- src/components/home-page/about-section/index.tsx
+++ src/components/home-page/about-section/index.tsx
@@ -2,9 +2,9 @@ import Card from 'components/common/card';
 const AboutSection = () => (
     <section
         title="about"
-        className=" flex mt-36 md:mt-0 bg-black px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48"
+        className=" mt-36 flex bg-black px-4 sm:px-8 md:mt-0 md:px-16 lg:px-32 xl:px-48"
     >
-        <div className="order-2 -mt-36 ml-7 md:-ml-52 md:mt-0 opacity-75 bg-blend-overlay w-3/4 md:w-1/2 absolute md:relative">
+        <div className="absolute order-2 -mt-36 ml-7 w-3/4 opacity-75 bg-blend-overlay md:relative md:-ml-52 md:mt-0 md:w-1/2">
             <img src="/back.webp" alt="" />
         </div>
 
