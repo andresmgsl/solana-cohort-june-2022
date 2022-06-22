acaac97c7007c6b0190dac165df0952dbed89159
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-22 02:35:34 +0200

    changing the color of the stats section with the new color

--- src/components/home-page/stats-section/index.tsx
+++ src/components/home-page/stats-section/index.tsx
@@ -3,22 +3,19 @@ const StatsSection = () => (
         title="faq"
         className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
     >
-        {/* <div className="flex h-64 items-center justify-center bg-[#63f194]">
-            <h2 className="text-lg text-white">Stats</h2>
-        </div> */}
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#63f194] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
+        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
             <span className="text-[21px] font-light  md:pr-6">Bounties1</span>
             <span className="text-[45px] font-medium">135</span>
         </div>
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#63f194] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:py-0 md:pl-0 lg:border-r-2">
+        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:py-0 md:pl-0 lg:border-r-2">
             <span className="text-[21px] font-light  md:pr-6">Bounties2</span>
             <span className="text-[45px] font-medium">135</span>
         </div>
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#63f194] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
+        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
             <span className="text-[21px] font-light  md:pr-6">Bounties3</span>
             <span className="text-[45px] font-medium">135</span>
         </div>
-        <div className="flex w-full flex-col bg-[#63f194] py-5 pl-7 md:flex-row md:items-center md:justify-center md:py-0 md:pl-0">
+        <div className="flex w-full flex-col bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:py-0 md:pl-0">
             <span className="text-[21px] font-light md:pr-6">Bounties4</span>
             <span className="text-[45px] font-medium">135</span>
         </div>
