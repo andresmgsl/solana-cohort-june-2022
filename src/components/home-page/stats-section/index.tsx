20a8303b89bf278d7f510929e5f7b40967577d8a
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-22 03:06:50 +0200

    refactor stats component

--- src/components/home-page/stats-section/index.tsx
+++ src/components/home-page/stats-section/index.tsx
@@ -1,24 +1,41 @@
+import StatItem from 'components/common/stat-item';
 const StatsSection = () => (
     <section
         title="faq"
         className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
     >
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
-            <span className="text-[21px] font-light  md:pr-6">Bounties1</span>
-            <span className="text-[45px] font-medium">135</span>
-        </div>
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:py-0 md:pl-0 lg:border-r-2">
-            <span className="text-[21px] font-light  md:pr-6">Bounties2</span>
-            <span className="text-[45px] font-medium">135</span>
-        </div>
-        <div className="flex w-full flex-col border-b-2 border-black bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:border-b-0 md:border-r-2 md:py-0 md:pl-0">
-            <span className="text-[21px] font-light  md:pr-6">Bounties3</span>
-            <span className="text-[45px] font-medium">135</span>
-        </div>
-        <div className="flex w-full flex-col bg-[#F07546] py-5 pl-7 md:flex-row md:items-center md:justify-center md:py-0 md:pl-0">
-            <span className="text-[21px] font-light md:pr-6">Bounties4</span>
-            <span className="text-[45px] font-medium">135</span>
-        </div>
+        <StatItem
+            backgroundColor="#F07546"
+            bountyName="Bounties1"
+            bountyNumber="135"
+            borderBottom="true"
+            borderRight="true"
+            removeBorderRightMedium="false"
+        />
+        <StatItem
+            backgroundColor="#F07546"
+            bountyName="Bounties2"
+            bountyNumber="135"
+            borderBottom="true"
+            borderRight="true"
+            removeBorderRightMedium="true"
+        />
+        <StatItem
+            backgroundColor="#F07546"
+            bountyName="Bounties3"
+            bountyNumber="135"
+            borderBottom="true"
+            borderRight="true"
+            removeBorderRightMedium="false"
+        />
+        <StatItem
+            backgroundColor="#F07546"
+            bountyName="Bounties4"
+            bountyNumber="135"
+            borderBottom="false"
+            borderRight="false"
+            removeBorderRightMedium="false"
+        />
     </section>
 );
 
