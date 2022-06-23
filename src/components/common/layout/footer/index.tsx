78de3c8102642dcf5dbc6bfe833ee17e5b9d536b
Hares Mahmood <hmahmood229@gmail.com>
2022-06-23 14:20:39 +0000

    Ensure correct `Card` `prop`s are used in `Footer`-component.

--- src/components/common/layout/footer/index.tsx
+++ src/components/common/layout/footer/index.tsx
@@ -3,9 +3,9 @@ import Card from "components/common/card";
 const Footer = () => (
     <footer className="my-16 flex flex-col justify-center gap-16 px-4 md:px-16 lg:px-32 xl:px-48">
         {/* draw a thin grey line */}
-        <div className="h-[1.5px] w-full bg-gray-600" />
-        <Card className="w-full rounded-xl bg-white p-7" blur="true">
-            <p className="text-white text-right text-sm font-light uppercase">
+        <div className="h-[1px] w-full bg-white" />
+        <Card className="w-full rounded-xl p-7" blur="true">
+            <p className="text-white font-light text-right text-sm uppercase">
                 Powered by <span className="text-[#d7753e] font-medium">Solana</span>
             </p>
         </Card>
