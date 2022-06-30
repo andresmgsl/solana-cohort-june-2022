cc5fab1080e5c79373e28f97d3ac5ed66da90e54
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-30 16:47:17 +0200

    refactor the stat section

--- src/components/home-page/stats-section/index.tsx
+++ src/components/home-page/stats-section/index.tsx
@@ -1,37 +1,56 @@
 import StatItem from 'components/common/stat-item';
+let StatItems = [
+    {
+        bountyName: 'Bounties1',
+        bountyNumber: '135',
+        borderBottom: 'true',
+        borderRight: 'true',
+        removeBorderRightMedium: 'false',
+    },
+    {
+        bountyName: 'Bounties2',
+        bountyNumber: '135',
+        borderBottom: 'true',
+        borderRight: 'true',
+        removeBorderRightMedium: 'true',
+    },
+    {
+        bountyName: 'Bounties3',
+        bountyNumber: '1000',
+        borderBottom: 'true',
+        borderRight: 'true',
+        removeBorderRightMedium: 'false',
+    },
+    {
+        bountyName: 'Bounties4',
+        bountyNumber: '135',
+        borderBottom: 'false',
+        borderRight: 'false',
+        removeBorderRightMedium: 'false',
+    },
+];
 const StatsSection = () => (
     <section
         title="faq"
         className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
     >
-        <StatItem
-            bountyName="Bounties1"
-            bountyNumber="135"
-            borderBottom="true"
-            borderRight="true"
-            removeBorderRightMedium="false"
-        />
-        <StatItem
-            bountyName="Bounties2"
-            bountyNumber="135"
-            borderBottom="true"
-            borderRight="true"
-            removeBorderRightMedium="true"
-        />
-        <StatItem
-            bountyName="Bounties3"
-            bountyNumber="1000"
-            borderBottom="true"
-            borderRight="true"
-            removeBorderRightMedium="false"
-        />
-        <StatItem
-            bountyName="Bounties4"
-            bountyNumber="135"
-            borderBottom="false"
-            borderRight="false"
-            removeBorderRightMedium="false"
-        />
+        {StatItems.map(
+            ({
+                bountyName,
+                bountyNumber,
+                borderBottom,
+                borderRight,
+                removeBorderRightMedium,
+            }) => (
+                <StatItem
+                    bountyName={bountyName}
+                    bountyNumber={bountyNumber}
+                    borderBottom={borderBottom}
+                    borderRight={borderRight}
+                    removeBorderRightMedium={removeBorderRightMedium}
+                />
+            ),
+        )}
     </section>
 );
 
