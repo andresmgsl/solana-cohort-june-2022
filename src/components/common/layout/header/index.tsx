d49b8646c7f057cd0baa8e74180b5050c004c535
Hares Mahmood <hmahmood229@gmail.com>
2022-07-04 17:10:26 +0000

    Add new design elements to `Header`.

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,7 +1,9 @@
+import Button from 'components/common/button';
+import SearchBar from 'components/common/search-bar';
 import Image from 'next/image';
 
 const Header = () => (
-    <header className="fixed top-0 z-50 flex h-16 w-full items-center bg-black/25 px-4 backdrop-blur-xl backdrop-filter">
+    <header className="flex flex-row justify-between fixed top-0 z-50 h-16 w-full items-center bg-black/25 px-4 backdrop-blur-xl backdrop-filter">
         <Image
             src="/logo.svg"
             alt="Solana Logo"
@@ -9,6 +11,14 @@ const Header = () => (
             width={175}
             height={100}
         />
+
+        <div className="flex flex-row gap-3 h-fit">
+            <SearchBar />
+            <div className="flex flex-row gap-3">
+                <Button text="Log in" variant="transparent" />
+                <Button text="Sign up" variant="orange" />
+            </div>
+        </div>
     </header>
 );
 
---
+++ src/components/common/search-bar/index.tsx
@@ -0,0 +1,11 @@
+import Card from "components/common/card";
+import Chip from "components/common/chip";
+
+const SearchBar = () => (
+    <Card className="flex flex-row w-fit h-full !rounded-full" blur="true">
+        <p className="text-white/50 text-sm uppercase tracking-wide"> Search </p>
+        <Chip value="CTRL + K" />
+    </Card>
+);
+
+export default SearchBar;
