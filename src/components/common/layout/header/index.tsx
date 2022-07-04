3e7afe50502b1893ac0c2e93d4dd386be5ec8d1f
Hares Mahmood <hmahmood229@gmail.com>
2022-07-04 17:56:40 +0000

    Add basic navigation.

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -2,29 +2,37 @@ import Button from 'components/common/button';
 import SearchBar from 'components/common/search-bar';
 // import Image from 'components/common/image';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => (
     <header className="flex flex-row justify-between fixed top-0 z-50 w-full h-20 items-center bg-black/25 px-6 backdrop-blur-xl backdrop-filter">
-        <Image
-            src="/logo.svg"
-            alt="logo"
-            width={175}
-            height={27}
-            // priority
-        />
+        <div className="flex gap-10">
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                width={175}
+                height={27}
+                // priority
+            />
+
+            <div className="flex flex-row gap-3 text-white">
+                <Link href="./"> Home </Link>
+                <Link href="./explorer"> Explorer </Link>
+            </div>
+        </div>
 
         <div className="flex-row gap-5 h-fit hidden sm:flex">
             <SearchBar />
 
             <div className="w-px bg-white" />
 
-            <div className="flex flex-row gap-3">
+            <div className="flex-row gap-3 hidden md:flex">
                 <Button text="Log in" variant="transparent" />
                 <Button text="Sign up" variant="orange" />
             </div>
         </div>
 
-        <div className="inline sm:hidden">
+        <div className="inline md:hidden">
             <Button text="☰" variant="transparent" />
         </div>
     </header>
