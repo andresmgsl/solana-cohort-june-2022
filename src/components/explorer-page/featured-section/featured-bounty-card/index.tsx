9b16e4277df58577a9c7fb91accc53b3119abee5
Hares Mahmood <hmahmood229@gmail.com>
2022-07-04 12:46:10 +0000

    Create `FeaturedBounty` carousel section.

--- src/components/explorer-page/featured-section/featured-bounty-card/index.tsx
+++ src/components/explorer-page/featured-section/featured-bounty-card/index.tsx
@@ -2,21 +2,21 @@ import Card from "components/common/card";
 import Chip from "components/common/chip";
 
 const FeaturedBountyCard = () => (
-    <Card className="flex flex-row w-fit max-h-48 p-6" blur="true">
-        <div className="flex flex-col justify-between w-fit h-full">
-            <div className="flex flex-col">
-                <h2 className="text-3xl font-medium">
+    <Card className="flex flex-row gap-6 w-max max-h-48 p-6" blur="true">
+        <div className="flex flex-col justify-between max-w-xs h-full">
+            <div className="flex flex-col whitespace-nowrap">
+                <h2 className="text-3xl text-ellipsis font-medium text-white">
                     Bounty name
                 </h2>
                 <h2 className="text-2xl font-medium text-primary">
                     $10,000,000
                 </h2>
             </div>
-            <div className="flex flex-row flex-wrap gap-1.5 w-full">
-                <Chip highlightValue="4" value="Participants" />
-                <Chip highlightValue="4" value="Participants" />
-                <Chip highlightValue="4" value="Participants" />
-                <Chip highlightValue="4" value="Participants" />
+            <div className="flex flex-row flex-wrap justify-start items-end gap-1.5 w-full">
+                <Chip highlightValue="4" value="Hunters" />
+                <Chip highlightValue="4" value="Funders" />
+                <Chip value="Category" />
+                <Chip value="GitHub Tag" />
             </div>
         </div>
         <div className="h-full aspect-square bg-white rounded-lg" />
--- src/components/explorer-page/featured-section/index.tsx
+++ src/components/explorer-page/featured-section/index.tsx
@@ -3,9 +3,14 @@ import FeaturedBountyCard from './featured-bounty-card';
 const FeaturedSection = () => (
     <section
         title="featured"
-        className="mt-24 w-full bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20"
+        className="flex flex-col gap-7 mt-24 w-full bg-gradient-to-tr from-primary/75 to-secondary/75 p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20"
     >
-        <div className="flex w-full flex-row gap-5">
+        <p className="text-sm text-white uppercase font-light"> Featured </p>
+        <h2 className="text-4xl font-medium text-white md:text-6xl">
+            Popular Bounties
+        </h2>
+        <div className="flex flex-row gap-5 w-full overflow-x-auto scroll-smooth snap-x">
+            <FeaturedBountyCard />
             <FeaturedBountyCard />
             <FeaturedBountyCard />
         </div>
