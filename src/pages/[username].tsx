3409342f6961674db44a84ea5c756b7b63bfd325
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-06 12:29:37 -0500

    Add `OpenBounties` to Explorer and Profile pages

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,10 +1,15 @@
 import Hero from 'components/profile-page/hero';
 import { NextPage } from 'next';
+import OpenBounties from 'components/explorer-page/open-bounties-section';
 
 const Profile: NextPage = () => (
     <div className="mt-8">
-        <div className="px-4 sm:px-8 md:gap-32 md:px-16 lg:px-32 xl:px-48">
+        <div className="flex flex-col gap-16 px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48">
             <Hero />
+            <div className="flex flex-col gap-7">
+                <h2 className="text-2xl font-medium text-white">My Bounties</h2>
+                <OpenBounties />
+            </div>
         </div>
     </div>
 );
--- src/pages/explorer.tsx
+++ src/pages/explorer.tsx
@@ -1,10 +1,22 @@
 import FeaturedSection from 'components/explorer-page/featured-section';
 import { NextPage } from 'next';
+import OpenBounties from 'components/explorer-page/open-bounties-section';
 
 const Explorer: NextPage = () => (
-    <>
+    <div className="flex flex-col gap-12">
         <FeaturedSection />
-    </>
+        <div className="flex flex-col gap-0">
+            <div className="flex w-full flex-col gap-7 px-5 sm:px-8 md:px-16 lg:px-32 xl:px-48">
+                <p className="text-sm font-light uppercase text-white">
+                    Browse
+                </p>
+                <h2 className="text-4xl font-medium text-white md:text-6xl">
+                    Open Bounties
+                </h2>
+                <OpenBounties />
+            </div>
+        </div>
+    </div>
 );
 
 export default Explorer;
