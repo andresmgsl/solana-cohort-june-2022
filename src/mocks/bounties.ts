e0d7bd79ebfb903c97c84b6567dce60c3f2988a3
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-11 17:23:09 -0500

    Create mock Bounty data

---
+++ src/mocks/bounties.ts
@@ -0,0 +1,9 @@
+import { Bounty } from 'types/bounty';
+
+export const mockBounties: Bounty[] = [
+    { name: 'Bounty A', prize: 10_000_000, thumbnail: '/dummyImg.png' },
+    { name: 'Bounty B', prize: 9_000_000, thumbnail: '/dummyImg.png' },
+    { name: 'Bounty C', prize: 8_000_000, thumbnail: '/dummyImg.png' },
+    { name: 'Bounty D', prize: 7_000_000, thumbnail: '/dummyImg.png' },
+    { name: 'Bounty E', prize: 6_000_000, thumbnail: '/dummyImg.png' },
+];
