bfc446a0bde00da66edae9a2ec4c56afd31ee4f0
Hares Mahmood <hmahmood229@gmail.com>
2022-07-11 17:36:17 +0000

    Replace text in `FeaturedSection`.

--- src/components/explorer-page/featured-section/index.tsx
+++ src/components/explorer-page/featured-section/index.tsx
@@ -1,14 +1,17 @@
 import FeaturedBountyCard from './featured-bounty-card';
+import Text from 'components/common/text';
 
 const FeaturedSection = () => (
     <section
         title="featured"
-        className="flex w-full flex-col gap-7 bg-gradient-to-tr from-primary/75 to-secondary/75 p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20"
+        className="flex w-full flex-col gap-7 bg-gradient-to-tr from-primary/75 to-secondary/75 text-white p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20"
     >
-        <p className="text-sm font-light uppercase text-white"> Featured </p>
-        <h2 className="text-4xl font-medium text-white md:text-6xl">
+        <Text variant="label">
+              Featured      
+        </Text>
+        <Text variant="big-heading">
             Popular Bounties
-        </h2>
+        </Text>
         <div className="flex flex-row gap-5 w-full overflow-x-auto scroll-smooth">
             {/* Create 4 FeaturedBountyCard components with 4 tags each */}
             <FeaturedBountyCard
