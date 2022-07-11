c4c0745d23e362da9536e9d7b9c1308f2cb97f8d
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-11 14:11:53 -0500

    Enable search input

--- src/components/common/filter-section/index.tsx
+++ src/components/common/filter-section/index.tsx
@@ -7,8 +7,8 @@ import {
 
 const FilterSection = () => (
     <>
-        <div className="flex flex-col gap-5 text-sm text-white md:flex-row md:gap-2">
-            <div className="flex max-h-40 grow rounded-full border bg-base/75 px-2 text-white">
+        <div className="flex flex-col gap-4 text-sm text-white md:flex-row md:gap-2">
+            <div className="flex max-h-40 grow rounded-full border bg-base/75 text-white">
                 <div className="flex w-full justify-between">
                     <div className="flex w-1/2 items-center justify-between border-r px-4">
                         <div className="flex items-center gap-2 py-3">
@@ -30,13 +30,15 @@ const FilterSection = () => (
                     </div>
                 </div>
             </div>
-            <div className="flex max-h-40 justify-start rounded-full border bg-base/75 px-2 text-white md:justify-center">
-                <div className="flex items-center gap-2 py-3 px-4">
+            <div className="relative">
+                <div className="pointer-events-none absolute inset-y-0 flex items-center pl-4">
                     <MdOutlineSearch className="h-4 w-4" />
-                    <p className="uppercase tracking-wide text-base-content opacity-50">
-                        Search for bounties
-                    </p>
                 </div>
+                <input
+                    className="block w-full rounded-full border border-white bg-base/75 py-3 pl-10 pr-6 uppercase tracking-wide placeholder:overflow-visible placeholder:text-base-content placeholder:opacity-50 focus:border-orange-400 focus:outline-none focus:ring-1 focus:ring-orange-400"
+                    placeholder="Search for bounties"
+                    type="text"
+                />
             </div>
         </div>
         <div className="flex justify-between pt-4 text-base-content">
