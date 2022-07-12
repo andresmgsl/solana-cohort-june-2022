2098bbebd37c9750f093124df581d80d7a4c2056
Hares Mahmood <hmahmood229@gmail.com>
2022-07-12 17:18:44 +0000

    Update styling in `OpenBounties`.

--- src/components/explorer-page/open-bounties-section/index.tsx
+++ src/components/explorer-page/open-bounties-section/index.tsx
@@ -1,40 +1,70 @@
+import Text from 'components/common/text';
 import FilterPart from 'components/common/filter-section';
-import BountyCard from 'components/common/main-bounty-card';
 import FeaturedBountyCard from '../featured-section/featured-bounty-card';
 
 const OpenBounties = () => (
     <section className="flex flex-col gap-4">
-        <div>
-            <FilterPart />
-           
+        <FilterPart />
+        <div className="px-6 flex-row justify-between gap-5 text-base-content hidden 2lg:flex">
+            <Text variant="label" className="w-full"> Basics </Text>
+            <div className="flex justify-between w-full">
+                <Text variant="label"> Reward · SOL </Text>
+                <Text variant="label"> Tags </Text>
+            </div>
         </div>
         <div className="flex flex-col gap-6">
-            <BountyCard
-                imageSource="/dummyImg.png"
-                bountyTitle="Bounty title"
-                bountyPrize="$10,000,000"
+            <FeaturedBountyCard
+                name="Really long bounty name"
+                reward="300"
+                tags={[
+                    { value: 'landing-page' },
+                    { value: 'enhancement' },
+                    { value: 'explorer-page' },
+                    { value: 'bug' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                ]}
             />
-            <BountyCard
-                imageSource="/dummyImg.png"
-                bountyTitle="Bounty title"
-                bountyPrize="$10,000,000"
+            <FeaturedBountyCard
+                name="Really long bounty name"
+                reward="1,000,000"
+                tags={[
+                    { value: 'landing-page' },
+                    { value: 'enhancement' },
+                    { value: 'explorer-page' },
+                    { value: 'bug' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                ]}
             />
-            <BountyCard
-                imageSource="/dummyImg.png"
-                bountyTitle="Bounty title"
-                bountyPrize="$10,000,000"
+            <FeaturedBountyCard
+                name="Really long bounty name"
+                reward="3"
+                tags={[
+                    { value: 'landing-page' },
+                    { value: 'enhancement' },
+                    { value: 'explorer-page' },
+                    { value: 'bug' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                ]}
             />
-            <BountyCard
-                imageSource="/dummyImg.png"
-                bountyTitle="Bounty title"
-                bountyPrize="$10,000,000"
+            <FeaturedBountyCard
+                name="Really long bounty name"
+                reward="57"
+                tags={[
+                    { value: 'landing-page' },
+                    { value: 'enhancement' },
+                    { value: 'explorer-page' },
+                    { value: 'bug' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                    { value: 'explorer-page' },
+                ]}
             />
-            <BountyCard
-                imageSource="/dummyImg.png"
-                bountyTitle="Bounty title"
-                bountyPrize="$10,000,000"
-            />
-
             <FeaturedBountyCard
                 name="Really long bounty name"
                 reward="300"
