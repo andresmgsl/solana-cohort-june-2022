fcc6f53111e845993c74e094fbf7aef03a7748d6
Hares Mahmood <hmahmood229@gmail.com>
2022-07-14 15:43:18 +0000

    Make `SearchBar` design more consistent with Figma.

--- src/components/common/search-bar/index.tsx
+++ src/components/common/search-bar/index.tsx
@@ -1,11 +1,16 @@
 import Card from "components/common/card";
 import Chip from "components/common/chip";
+import Text from "../text";
+
+import { MdOutlineSearch } from "react-icons/md";
 
 const SearchBar = () => (
-    <Card className="flex flex-row gap-5 w-fit max-h-full px-5 py-3 !rounded-full cursor-pointer">
-        <p className="text-white/50 text-sm uppercase tracking-wide"> Search </p>
+    <Card className="flex flex-row items-center gap-5 w-fit max-h-full px-5 py-3 !rounded-full cursor-pointer">
+        <MdOutlineSearch className="h-4 aspect-square" />
+        <Text variant="label" className="text-secondary"> Search </Text>
         <Chip value="CTRL + K" />
     </Card>
 );
 
 export default SearchBar;
+4
