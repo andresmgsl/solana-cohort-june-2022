b8a36e228f244369ba20be01c0ab560173cee6dc
Hares Mahmood <hmahmood229@gmail.com>
2022-07-14 16:52:05 +0000

    Add basic `SideNavigation`.

--- src/components/common/layout/header/side-navigation/index.tsx
+++ src/components/common/layout/header/side-navigation/index.tsx
@@ -3,20 +3,10 @@ import Button from 'components/common/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import SearchBar from 'components/common/search-bar';
-import SideNavigation from './side-navigation';
-
 import { MdMenu } from 'react-icons/md';
 
-const Header = () => (
-    <header className="sticky top-0 z-50 flex h-20 w-full flex-row items-center justify-between bg-black/25 px-6 backdrop-blur-xl backdrop-filter border-b-1.5 border-b-line">
-        <Image
-            src="/logo.svg"
-            alt="logo"
-            width={175}
-            height={27}
-            // priority
-        />
-
+const SideNavigation = () => (
+    <div className="fixed top-0 z-50 flex h-screen w-1/3 flex-row items-center justify-between bg-black/25 px-6 backdrop-blur-xl backdrop-filter border-b-1.5 border-b-line">
         <div className="hidden h-fit flex-row items-center gap-5 text-white md:flex">
             <div className="flex flex-row gap-3">
                 <Link href="./">
@@ -44,7 +34,7 @@ const Header = () => (
                 <MdMenu className="h-4 aspect-square" />
             </Button>
         </div>
-    </header>
+    </div>
 );
 
-export default Header;
+export default SideNavigation;
