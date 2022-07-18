ddc3fec0e5b161d70cda6351b098a9a42775e2a4
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-18 12:16:38 -0500

    Render `FeaturedSection` Bounties dynamically

--- src/components/explorer-page/featured-section/index.tsx
+++ src/components/explorer-page/featured-section/index.tsx
@@ -1,44 +1,22 @@
 import FeaturedBountyCard from '../bounty-card';
 import Text from 'components/common/text';
+import { mockBounties } from 'mocks/bounties';
 
 const FeaturedSection = () => (
     <section
         title="featured"
         className="flex w-full flex-col gap-7 bg-gradient-to-tr from-primary/75 to-secondary/75 text-white p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20"
     >
-        <Text variant="label">
-              Featured      
-        </Text>
-        <Text variant="big-heading">
-            Popular Bounties
-        </Text>
-        <div className="flex flex-row justify-start gap-5 w-full overflow-x-auto scroll-smooth">
-            <FeaturedBountyCard
-                name="Really long bounty name"
-                reward="300"
-                tags={[
-                    { value: 'landing-page' },
-                    { value: 'enhancement' },
-                    { value: 'explorer-page' },
-                    { value: 'bug' },
-                    { value: 'explorer-page' },
-                    { value: 'explorer-page' },
-                    { value: 'explorer-page' },
-                ]}
-                responsive={false}
-            />
-
-            <FeaturedBountyCard
-                name="Really long bounty name"
-                reward="10,000,000"
-                tags={[
-                    { value: 'landing-page' },
-                    { value: 'bug' },
-                    { value: 'explorer-page' },
-                    { value: 'enhancement' }
-                ]}
-                responsive={false}
-            />
+        <Text variant="label">Featured</Text>
+        <Text variant="big-heading">Popular Bounties</Text>
+        <div className="flex w-full flex-row justify-start gap-5 overflow-x-auto scroll-smooth">
+            {mockBounties.map((bounty, index) => (
+                <FeaturedBountyCard
+                    key={index}
+                    responsive={false}
+                    {...bounty}
+                />
+            ))}
         </div>
     </section>
 );
