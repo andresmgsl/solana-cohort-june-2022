642ba4d94c2daa8d568bf5e38799d53c9f60d348
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-20 18:17:34 -0500

    Add server-side authentication to `Profile`

--- src/components/profile-page/hero/index.tsx
+++ src/components/profile-page/hero/index.tsx
@@ -2,43 +2,44 @@
 import Link from 'next/link';
 import { MdOutlineSettings } from 'react-icons/md';
 import Stars from './stars';
-import { useRouter } from 'next/router';
 
-const Hero = () => {
-    const {
-        query: { username },
-    } = useRouter();
+type HeroProps = {
+    fullName: string;
+    isOwnProfile: boolean;
+    username: string;
+};
 
-    return (
-        <div className="flex flex-col">
-            <div className="h-60 w-full rounded-tl-[theme(spacing.24)] bg-gradient-to-tr from-primary/75 to-secondary/75" />
-            <div className="flex flex-row justify-between gap-12 px-2 md:px-6">
-                <div className="flex flex-col gap-6 md:flex-row md:gap-10">
-                    <img
-                        alt="avatar"
-                        className="-mt-7 flex aspect-square h-32 w-max rounded-full border-4 border-white md:h-36"
-                        src="avatar.png"
-                    />
-                    <div className="flex flex-col gap-3 md:mt-6">
-                        <div className="flex flex-row flex-wrap items-center gap-x-4 gap-y-2">
-                            <h2 className="text-3xl font-medium text-white">
-                                John Doe
-                            </h2>
-                            <p className="text-sm uppercase tracking-wide text-primary">
-                                @{username}
-                            </p>
-                        </div>
-                        <Stars rating={3} size="md" />
+const Hero = ({ fullName, isOwnProfile, username }: HeroProps) => (
+    <div className="flex flex-col">
+        <div className="h-60 w-full rounded-tl-[theme(spacing.24)] bg-gradient-to-tr from-primary/75 to-secondary/75" />
+        <div className="flex flex-row justify-between gap-12 px-2 md:px-6">
+            <div className="flex flex-col gap-6 md:flex-row md:gap-10">
+                <img
+                    alt="avatar"
+                    className="-mt-7 flex aspect-square h-32 w-max rounded-full border-4 border-white md:h-36"
+                    src="avatar.png"
+                />
+                <div className="flex flex-col gap-3 md:mt-6">
+                    <div className="flex flex-row flex-wrap items-center gap-x-4 gap-y-2">
+                        <h2 className="text-3xl font-medium text-white">
+                            {fullName}
+                        </h2>
+                        <p className="text-sm uppercase tracking-wide text-primary">
+                            @{username}
+                        </p>
                     </div>
+                    <Stars rating={3} size="md" />
                 </div>
-                <div className="mt-6">
+            </div>
+            <div className="mt-6">
+                {isOwnProfile && (
                     <Link href="/settings" passHref>
                         <MdOutlineSettings color="#999999" size={32} />
                     </Link>
-                </div>
+                )}
             </div>
         </div>
-    );
-};
+    </div>
+);
 
 export default Hero;
--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,18 +1,62 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { getUser, withPageAuth } from '@supabase/auth-helpers-nextjs';
+
 import BountyList from 'components/common/bounty-list';
 import Hero from 'components/profile-page/hero';
-import { NextPage } from 'next';
+import { User } from 'types/user';
 import { mockBounties } from 'mocks/bounties';
+import { mockUser } from 'mocks/user';
+import { useRouter } from 'next/router';
 
-const Profile: NextPage = () => (
-    <div className="mt-8">
-        <div className="flex flex-col gap-16 px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48">
-            <Hero />
-            <div className="flex flex-col gap-7">
-                <h2 className="text-2xl font-medium text-white">My Bounties</h2>
-                <BountyList bounties={mockBounties} />
+type ProfilePageProps = {
+    authenticatedUser: User;
+};
+
+const ProfilePage: NextPage<ProfilePageProps> = ({ authenticatedUser }) => {
+    const { query } = useRouter();
+
+    const isOwnProfile = query.username === authenticatedUser.username;
+
+    const heroProps = {
+        isOwnProfile,
+        ...(isOwnProfile ? authenticatedUser : mockUser),
+    };
+
+    const fullName = isOwnProfile
+        ? authenticatedUser.fullName
+        : mockUser.fullName;
+    const firstName = fullName.split(' ')[0];
+    const bountyListTitlePronoun = isOwnProfile ? 'My' : `${firstName}'s`;
+
+    return (
+        <div className="mt-8">
+            <div className="flex flex-col gap-16 px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48">
+                <Hero {...heroProps} />
+                <div className="flex flex-col gap-7">
+                    <h2 className="text-2xl font-medium text-white">
+                        {bountyListTitlePronoun} Bounties
+                    </h2>
+                    <BountyList bounties={mockBounties} />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
-export default Profile;
+export default ProfilePage;
+
+export const getServerSideProps: GetServerSideProps = withPageAuth({
+    async getServerSideProps(context) {
+        const { user } = await getUser(context);
+        const userMetadata = user?.user_metadata;
+
+        return {
+            props: {
+                authenticatedUser: {
+                    fullName: userMetadata.full_name,
+                    username: userMetadata.user_name,
+                },
+            },
+        };
+    },
+});
