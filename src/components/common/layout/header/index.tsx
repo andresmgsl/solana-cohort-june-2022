cfbbd1dbf8ed8aff94efd28244faeb7d37c847c9
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-20 18:03:44 -0500

    Add client-side authentication to `Header`

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,50 +1,67 @@
 import Button from 'components/common/button';
-// import Image from 'components/common/image';
 import Image from 'next/image';
-import Text from 'components/common/text';
 import Link from 'next/link';
-import SearchBar from 'components/common/search-bar';
-import SideNavigation from './side-navigation';
-import { useState } from 'react';
-import { cn } from 'utils';
-
 import { MdMenu } from 'react-icons/md';
 import NavElement from './nav-element';
+import SearchBar from 'components/common/search-bar';
+import Text from 'components/common/text';
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
+import { useUser } from '@supabase/auth-helpers-react';
 
-const Header = () => (
-    <header className="sticky top-0 z-50 flex h-20 w-full flex-row items-center justify-between bg-black/25 px-6 backdrop-blur-xl backdrop-filter border-b-1.5 border-b-line">
-        <Image
-            src="/logo.svg"
-            alt="logo"
-            width={175}
-            height={27}
-            // priority
-        />
+const Header = () => {
+    const { user } = useUser();
 
-        <div className="hidden h-fit flex-row items-center gap-5 text-white md:flex">
-            <div className="flex flex-row gap-7">
-                {/* TODO Add `TabbedLayout? */}
-                <NavElement label="home" href="/" />
-                <NavElement label="explorer" href="/explorer" />
+    const signIn = async () => {
+        await supabaseClient.auth.signIn({ provider: 'github' });
+    };
+
+    const signOut = async () => {
+        await supabaseClient.auth.signOut();
+    };
+
+    return (
+        <header className="sticky top-0 z-50 flex h-20 w-full flex-row items-center justify-between border-b-1.5 border-b-line bg-black/25 px-6 backdrop-blur-xl backdrop-filter">
+            <Image src="/logo.svg" alt="logo" width={175} height={27} />
+
+            <div className="hidden h-fit flex-row items-center gap-5 text-white md:flex">
+                <div className="flex flex-row gap-7">
+                    {/* TODO Add `TabbedLayout? */}
+                    <NavElement label="home" href="/" />
+                    <NavElement label="explorer" href="/explorer" />
+                </div>
+
+                <div className="h-8 w-px bg-line" />
+
+                <SearchBar />
+
+                <div className="h-8 w-px bg-line" />
+
+                <div className="flex flex-row items-center gap-3">
+                    {user && (
+                        <Text variant="label">
+                            <Link
+                                href={`${user.user_metadata.user_name}`}
+                                passHref
+                            >
+                                {user.user_metadata.user_name}
+                            </Link>
+                        </Text>
+                    )}
+                    <Button
+                        onClick={user ? signOut : signIn}
+                        text={user ? 'Sign out' : 'Log in / Sign up'}
+                        variant="orange"
+                    />
+                </div>
             </div>
 
-            <div className="w-px h-8 bg-line" />
-
-            <SearchBar />
-
-            <div className="w-px h-8 bg-line" />
-
-            <div className="flex flex-row gap-3">
-                <Button text="Log in / Sign up" variant="orange" />
+            <div className="inline md:hidden">
+                <Button variant="transparent">
+                    <MdMenu className="aspect-square h-4" />
+                </Button>
             </div>
-        </div>
-
-        <div className="inline md:hidden">
-            <Button  variant="transparent">
-                <MdMenu className="h-4 aspect-square" />
-            </Button>
-        </div>
-    </header>
-);
+        </header>
+    );
+};
 
 export default Header;
