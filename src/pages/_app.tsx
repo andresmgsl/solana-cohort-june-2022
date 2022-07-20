927a87556965df26813bc96563cef9faa9474eac
hreyesm <hectorreyesmanrique@gmail.com>
2022-07-20 18:02:54 -0500

    Update _app.tsx, add `handleAuth()` API route for Supabase integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 import Layout from 'components/common/layout';
+import { UserProvider } from '@supabase/auth-helpers-react';
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+    <UserProvider supabaseClient={supabaseClient}>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    </UserProvider>
 );
 
 export default App;
