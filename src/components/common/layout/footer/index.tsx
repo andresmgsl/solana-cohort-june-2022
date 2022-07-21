15aa6b49532a893938e228f3034b13e2965bf17c
Hares Mahmood <hmahmood229@gmail.com>
2022-07-21 17:53:52 +0000

    Improve `Footer` design.
    Consistent with Figma prototype.

--- src/components/common/layout/footer/index.tsx
+++ src/components/common/layout/footer/index.tsx
@@ -1,14 +1,23 @@
 import Card from 'components/common/card';
+import Text from 'components/common/text';
+import Image from 'components/common/image';
 
 const Footer = () => (
-    <footer className="my-16 flex flex-col justify-center gap-16 px-4 md:px-16 lg:px-32 xl:px-48">
+    <footer className="my-16 flex flex-col justify-center gap-16 px-4 md:px-16 lg:px-32 xl:px-48 text-white">
         {/* draw a thin grey line */}
-        <div className="h-0.5 w-full bg-white" />
-        <Card className="w-full rounded-xl p-7" blur="true">
-            <p className="text-right text-sm font-light uppercase text-white">
+        <div className="h-px w-full bg-line"/>
+        <Card className="w-full flex flex-row justify-between rounded-xl p-7">
+            <Image
+                src="/logo-icon.svg"
+                alt="solana icon"
+                width={20}
+                height={17.89}
+                className="saturate-0"
+            />
+            <Text variant="label" className="font-light">
                 Powered by{' '}
                 <span className="font-medium text-primary">Solana</span>
-            </p>
+            </Text>
         </Card>
     </footer>
 );
