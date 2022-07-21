d145dbdd5e1d0d54a73451d388c57201a2a77d38
Hares Mahmood <hmahmood229@gmail.com>
2022-07-21 17:01:07 +0000

    Improve `Header` on mobile devices.
    - Clicking on the logo now routes to the `Landing`-page.
    - Decrease the gap between `NavElement`s.

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,29 +1,32 @@
 import Button from 'components/common/button';
 import Image from 'components/common/image';
 import SearchBar from 'components/common/search-bar';
+import Link from 'next/link';
 
 import { MdFace, MdSearch } from 'react-icons/md';
 import NavElement from './nav-element';
 
 const Header = () => (
     <header className="sticky top-0 z-50 flex h-20 w-full flex-row items-center justify-between bg-black/25 px-6 backdrop-blur-xl backdrop-filter border-b-1.5 border-b-line">
-        <div className="flex flex-row items-center gap-3 w-fit">
-            <Image
-                src="/logo-icon.svg"
-                alt="solana icon"
-                width={29.16}
-                height={26.08}
-            />
-            <Image
-                src="/logo-text.svg"
-                alt="solana text"
-                className="hidden md:inline"
-                width={134.46}
-                height={20.1}
-            />
-        </div>
+        <Link href="./">
+            <div className="flex flex-row items-center gap-3 w-fit">
+                <Image
+                    src="/logo-icon.svg"
+                    alt="solana icon"
+                    width={29.16}
+                    height={26.08}
+                />
+                <Image
+                    src="/logo-text.svg"
+                    alt="solana text"
+                    className="hidden md:inline"
+                    width={134.46}
+                    height={20.1}
+                />  
+            </div>
+        </Link>
         <div className="h-full flex flex-row items-center gap-5 text-white">
-            <div className="flex flex-row gap-3 sm:gap-7">
+            <div className="flex flex-row gap-5 sm:gap-7">
                 <NavElement label="home" href="/" />
                 <NavElement label="explorer" href="/explorer" />
             </div> 
