b0aebb51ff18464de60cfd1c3090e14201f22154
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-22 13:42:10 +0200

    center about section

--- src/components/home-page/about-section/index.tsx
+++ src/components/home-page/about-section/index.tsx
@@ -3,7 +3,7 @@ import Card from 'components/common/card';
 const AboutSection = () => (
     <section
         title="about"
-        className=" mt-36 flex bg-black px-4 sm:px-8 md:mt-0 md:px-16 lg:px-32 xl:px-48"
+        className="justify-center mt-36 flex bg-black px-4 sm:px-8 md:mt-0 md:px-16 lg:px-32 xl:px-48"
     >
         <div className="absolute order-2 -mt-36 ml-7 w-3/4 opacity-75 bg-blend-overlay md:relative md:-ml-52 md:mt-0 md:w-1/2">
             <img src="/back.webp" alt="" />
