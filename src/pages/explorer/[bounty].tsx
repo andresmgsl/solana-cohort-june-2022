f8293435ee619ecbf545849c4513de9c66588fea
Hares Mahmood <hmahmood229@gmail.com>
2022-07-22 16:50:35 +0000

    Add simple `Back`-button.

--- src/pages/explorer/[bounty].tsx
+++ src/pages/explorer/[bounty].tsx
@@ -1,8 +1,12 @@
+import Button from 'components/common/button';
 import { NextPage } from 'next';
+import { MdChevronLeft } from 'react-icons/md';
 
 const BountyDetail: NextPage = () => (
     <div className="mt-8">
-        
+        <Button text="Back" variant="outlined">
+            <MdChevronLeft className="h-4 aspect-square" />
+        </Button>
     </div>
 );
 
